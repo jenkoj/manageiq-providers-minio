83e56e63d3b31fd00c170e8ac424618e84ae1051
jakob_jenko <jakob.jenko@xlab.si>
2022-12-12 13:24:16 +0100

    Initialize inverntory to store fetched buckets.

--- app/models/manageiq/providers/minio/inventory/persister.rb
+++ app/models/manageiq/providers/minio/inventory/persister.rb
@@ -1,7 +1,3 @@
 class ManageIQ::Providers::Minio::Inventory::Persister < ManageIQ::Providers::Inventory::Persister
   require_nested :StorageManager
-
-  def initialize_inventory_collections
-    add_cloud_collection(:vms)
-  end
 end
--- app/models/manageiq/providers/minio/inventory/persister/storage_manager.rb
+++ app/models/manageiq/providers/minio/inventory/persister/storage_manager.rb
@@ -1,2 +1,7 @@
 class ManageIQ::Providers::Minio::Inventory::Persister::StorageManager < ManageIQ::Providers::Minio::Inventory::Persister
+  attr_reader :collections
+
+  def initialize_inventory_collections
+      add_collection(storage, :cloud_object_store_containers)
+  end
 end
