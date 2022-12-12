b487d14eed80438ce6a7d1764015868c98a47471
jakob_jenko <jakob.jenko@xlab.si>
2022-12-12 13:22:46 +0100

    Parse the response and insert buckets into db.

--- app/models/manageiq/providers/minio/inventory/parser/storage_manager.rb
+++ app/models/manageiq/providers/minio/inventory/parser/storage_manager.rb
@@ -1,2 +1,17 @@
 class ManageIQ::Providers::Minio::Inventory::Parser::StorageManager < ManageIQ::Providers::Minio::Inventory::Parser
+  attr_accessor :collector
+  
+  def parse
+    cloud_object_store_container
+  end 
+  
+  # Use collector class to fetch data from API and then store in into db using persister class. 
+  def cloud_object_store_container
+    collector.cloud_object_store_container.each do |container|
+      persister.cloud_object_store_containers.build(
+          :name => container["name"],
+          :ems_ref =>  container['creation_date'].to_s.gsub(" ","_") 
+      )
+    end
+  end
 end
