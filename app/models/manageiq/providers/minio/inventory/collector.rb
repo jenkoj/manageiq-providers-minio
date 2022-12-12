4e8fbc01007e7c9f4289d5b17592d9bc78faa178
jakob_jenko <jakob.jenko@xlab.si>
2022-12-12 13:21:03 +0100

    Added collector to fetch the buckets from minio.

--- app/models/manageiq/providers/minio/inventory/collector.rb
+++ app/models/manageiq/providers/minio/inventory/collector.rb
@@ -1,11 +1,3 @@
 class ManageIQ::Providers::Minio::Inventory::Collector < ManageIQ::Providers::Inventory::Collector
   require_nested :StorageManager
-
-  def connection
-    @connection ||= manager.connect
-  end
-
-  def vms
-    connection.vms
-  end
 end
--- app/models/manageiq/providers/minio/inventory/collector/storage_manager.rb
+++ app/models/manageiq/providers/minio/inventory/collector/storage_manager.rb
@@ -1,2 +1,13 @@
 class ManageIQ::Providers::Minio::Inventory::Collector::StorageManager < ManageIQ::Providers::Minio::Inventory::Collector
+    attr_accessor :manager
+
+    def connection
+        @connection ||= manager.connect
+        puts "connected to "+manager
+    end
+
+    def cloud_object_store_container
+        @cloud_object_store_container ||= manager.connect.list_buckets.flat_map(&:buckets)
+    end 
+
 end
